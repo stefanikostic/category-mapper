01e5e737350bc95bb0aa6478480a63f7d7f34a56
stefanikostic <stefanikostic6j@gmail.com>
2024-03-10 18:59:50 +0100

    Removed unnecessary scannning of elastic search client to fetch products and ProductsAPI.java.

--- category-mapper-api/src/main/java/com/shopcompare/categorymapper/CategoryMapperApplication.java
+++ category-mapper-api/src/main/java/com/shopcompare/categorymapper/CategoryMapperApplication.java
@@ -1,12 +1,11 @@
 package com.shopcompare.categorymapper;
 
-import com.shopcompare.elasticsearch.client.search.ElasticsearchClientSearchMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {CategoryMapperMarker.class, ElasticsearchClientSearchMarker.class})
+@ComponentScan(basePackageClasses = CategoryMapperMarker.class)
 public class CategoryMapperApplication {
 
 	public static void main(String[] args) {
